4783de817e572dc76a7a4263ef1640ba4a69b925
hai.nguyen <hai.nguyen@codihaus.com>
2025-06-04 14:02:26 +0700

    update test email

--- config/mailer.js
+++ config/mailer.js
@@ -3,11 +3,14 @@ const nodemailer = require('nodemailer');
 const { EMAIL_USER, EMAIL_PASS } = require('./config');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: EMAIL_USER,
     pass: EMAIL_PASS,
   },
 });
+
 
 module.exports = transporter;
